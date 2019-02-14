658a2b6bf8ae75aa9f9a0c96d2256e1b74cff931
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-14 10:14:41 +0100

    Zmiany

--- src/start/HelloWorld.java
+++ src/start/HelloWorld.java
@@ -1,4 +1,12 @@
 package start;
+//komentarze
+//public static void main(String[]args){}
+//sout System.out.println();
 
-public class HelloWorld {
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("hello World!");
+           }
 }
