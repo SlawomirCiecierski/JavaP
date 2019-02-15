8dd395593874a3ffde1aee63305e90579892f2ab
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-15 22:12:46 +0100

    15.02.2019 22:12

---
+++ src/start/Cw26.java
@@ -0,0 +1,20 @@
+package start;
+//Znajdź wszystkie pary liczb, których suma jest równa danej liczbie K (bez sortowania).
+public class Cw26 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int liczbaK=5;
+        int licznik1;
+        int licznik2;
+
+        for (licznik1 = 0; licznik1 < numbers.length; licznik1++) {
+            for (licznik2 = 0; licznik2 < numbers.length; licznik2++) {
+
+                if (numbers[licznik1] + numbers[licznik2]== liczbaK) {
+
+                    System.out.println("("+numbers[licznik1] +")+("+ numbers[licznik2]+")=("+liczbaK+")");
+                }
+            }
+        }
+    }
+}
