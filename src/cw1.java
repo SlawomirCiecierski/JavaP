b414b467ef8427f08d033fccdc9acdd7c59eb47d
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-15 22:13:32 +0100

    15.02.2019 22:17

---
+++ src/cw1.java
@@ -0,0 +1,28 @@
+public class cw1 {
+
+    public static void main(String[] args) {
+
+        String imie, nazwisko, stanowisko;
+        final String PESEL;
+        char plec;
+        double wynagrodzenieNetto;
+        boolean emeryt;
+
+        imie = "Sławomir";
+        System.out.println("imie = " + imie);
+        nazwisko = "Ciecierski";
+        System.out.println("nazwisko = " + nazwisko);
+        stanowisko = "kierownik";
+        System.out.println("stanowisko = " + stanowisko);
+        PESEL = "12345678912";
+        System.out.println("PESEL = " + PESEL);
+        plec = 'm';
+        System.out.println("plec = " + plec);
+        wynagrodzenieNetto = 3000;
+        System.out.println("wynagrodzenieNetto = " + wynagrodzenieNetto);
+        emeryt = true;
+        System.out.println("emeryt = " + emeryt);
+
+    }
+
+}
