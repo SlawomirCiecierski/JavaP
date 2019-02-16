f4664e5a34cf90df63b06a5504fbcca81d251977
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-16 15:21:23 +0100

    16.02.2019 15:25

---
+++ src/start/Cw27.java
@@ -0,0 +1,20 @@
+package start;
+
+//Znajdź najmniejszą sumę kolejnych K elementów w tabeli N liczb całkowitych
+public class Cw27 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 1, 1, 5, 6, 9, 8, 9};
+        int licznik;
+        int suma = 20;
+        for (licznik = 1; licznik < numbers.length; licznik++) {
+            System.out.println("(" + numbers[licznik - 1] + ")+(" + numbers[licznik] + ")=(" + (numbers[licznik - 1] + numbers[licznik]) + ")");
+            if (numbers[licznik - 1] + numbers[licznik] < suma) {
+                suma = (numbers[licznik - 1] + numbers[licznik]);
+            }
+
+        }
+        System.out.println("Najmniejsza suma: "  + suma);
+
+    }
+}
+
