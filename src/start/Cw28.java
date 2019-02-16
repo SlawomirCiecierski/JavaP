3f1a75828f4b83f120ed8538b7c982f28435cb86
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-16 17:05:40 +0100

    16.02.2019 17:05

---
+++ src/start/Cw28.java
@@ -0,0 +1,9 @@
+package start;
+//Znajdź ile razy występują ciągi powtarzających się liczb
+public class Cw28 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 2, 4, 1, 1, 1, 8, 9};
+        int ciag=0;
+        //ciąg powtarzających się liczb to {2,2} {1,1,1) - 2 ciągi
+    }
+}
